e29153c0daade5c800582bda40516459dcf1ac62
marsevilspirit <a226214089@qq.com>
2023-11-11 21:44:18 +0800

    update 1.9

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,7 +2,11 @@ return {
         "neovim/nvim-lspconfig",
         dependencies = { "williamboman/mason-lspconfig.nvim"},
         config = function()
-                require'lspconfig'.lua_ls.setup{}
-                require'lspconfig'.clangd.setup{}
+                require'lspconfig'.lua_ls.setup{
+                        capabilities = capabilities
+                }
+                require'lspconfig'.clangd.setup{
+                        capabilities = capabilities
+                }
         end,
 }
