dae33508dec7e217b9b587bead3434dd12d61af8
marsevilspirit <a2261214089@qq.com>
2023-12-11 22:12:17 +0800

    fix c-f

--- lua/plugins/flash.lua
+++
@@ -1,9 +0,0 @@
-return{
-  "folke/flash.nvim",
-  ---@typeFlash.Config
-  opts = {},
-  -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-  },
-}
