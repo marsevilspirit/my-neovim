c84247739a17a22412f3168b1ca8a02811271e4e
marsevilspirit <a2261214089@qq.com>
2023-12-11 14:59:39 +0800

    update

--- init.lua
+++ init.lua
@@ -12,16 +12,24 @@ require("basic")
 -- 快捷键映射
 require("keybindings")
 
+-- 自动保存
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+    callback = function()
+        vim.fn.execute("silent! write")
+        vim.notify("Autosaved!", vim.log.levels.INFO, {})
+    end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                "--branch=stable", -- latest stable release
-                lazypath,
-        })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
