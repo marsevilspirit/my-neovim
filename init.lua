ed73f000c65db229df27d04fef1f86404e06eee1
marsevilspirit <a2261214089@qq.com>
2023-12-11 15:06:29 +0800

    update

--- init.lua
+++ init.lua
@@ -20,6 +20,14 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
     end,
 })
 
+-- 取消新行注释
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+    end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -41,8 +49,6 @@ require("lazy").setup("plugins")
 
 -- colorscheme风格
 vim.cmd('colorscheme base16-snazzy')
-
-
 
 
 
