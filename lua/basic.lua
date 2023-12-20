75a1763540ddb27367a14759e4efbe6ec3fb178f
marsevilspirit <a2261214089@qq.com>
2023-12-20 18:22:03 +0800

    update

--- lua/basic.lua
+++ lua/basic.lua
@@ -2,8 +2,8 @@
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
 -- jkhl 移动时光标周围保留8行
-vim.o.scrolloff = 12
-vim.o.sidescrolloff = 12 
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8 
 -- 使用相对行号
 vim.wo.number = true
 vim.wo.relativenumber = false 
