64363a138b46ae1a44a382b7770e87e716e95a4f
marsevilspirit <marsevilspirit@gmail.com>
2024-08-21 10:56:19 +0800

    add comment.lua

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,16 @@
 return{
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require("bufferline").setup{
-        options = {
-            diagnostics = "nvim_lsp",
-            diagnostics_indicator = function(count, level)
-                local icon = level:match("error") and " " or " "
-                return " " .. icon .. count
-            end,
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        require("bufferline").setup{
+            options = {
+                diagnostics = "nvim_lsp",
+                diagnostics_indicator = function(count, level)
+                    local icon = level:match("error") and " " or " "
+                    return " " .. icon .. count
+                end,
+            }
         }
-    }
-  end,
+    end,
 }
